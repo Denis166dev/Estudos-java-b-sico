fa056c80db5a32fe5dddfb9d05b50e1af8915df6
Denis Willian <deniswillian166@gmail.com>
2024-08-23 10:03:25 -0300

    tipos de variaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "DENIS"; // Variável do tipo String
+        int idade = 18; // Variável do tipo inteiro
+        double peso = 60.0; // Variável do tipo double (número com ponto flutuante)
+        char sexo = 'M'; // Variável do tipo char (um único caractere)
+        boolean cursandoAds = true; // Variável do tipo boolean (verdadeiro ou falso)
     }
 }
